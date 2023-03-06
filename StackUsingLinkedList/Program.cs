da3801d944937151fc56fcf13eb2e2865294bfd5
BashaVali <bashavali222@gmail.com>
2023-03-07 02:44:24 +0530

    DataStructures_LinkedList

---
+++ StackUsingLinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using StackUsingLinkedList;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked Lis !");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+        }
+    }
+}
